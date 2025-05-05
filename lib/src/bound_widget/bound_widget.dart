ac471d444df51a8c3654b68310605fe8a6cb0825
mako <hyxabrod@gmail.com>
2025-05-05 22:18:42 +0300

    Fix BoundWidget generic type to extend BasicBinder instead of Binder

--- lib/src/bound_widget/bound_widget.dart
+++ lib/src/bound_widget/bound_widget.dart
@@ -2,7 +2,7 @@ import 'package:flutter/widgets.dart';
 import 'package:flutter_fbi/src/binder/binder_interface.dart';
 import 'package:flutter_fbi/src/bound_widget/bound_interface.dart';
 
-abstract class BoundWidget<B extends Binder> extends StatefulWidget implements IBoundWidget<B> {
+abstract class BoundWidget<B extends BasicBinder> extends StatefulWidget implements IBoundWidget<B> {
   final B binder;
   const BoundWidget({Key? key, required this.binder});
 
